e52fee09c0a0b717bd1dacc9fe93eac858132766
Sindre Moldsvor <sindre.moldsvor@zeabuz.com>
2025-10-11 14:46:33 +0200

    Changed url in dockerfile and added scripts

---
+++ run.sh
@@ -0,0 +1,9 @@
+docker run --init -it \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /etc/timezone:/etc/timezone:ro \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v ./bags:/root/ros2_ws/bags \
+  -v ./config:/root/ros2_ws/src/LIO-SAM/config \
+  -e DISPLAY=$DISPLAY \
+  lio-sam-ros2 \
+  bash
